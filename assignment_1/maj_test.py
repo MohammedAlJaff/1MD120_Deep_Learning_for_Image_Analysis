c8a948e23c3e50927145e5ad9d63c88bca9b5199
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-04-07 08:04:44 +0200

    check point 7thapril.

---
+++ assignment_1/maj_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import load_auto as load
+from maj_linear_model import LinearRegressionModel
+
+if __name__ =='__main__':
+    print('yay')
+    
+    maj_model = LinearRegressionModel()
